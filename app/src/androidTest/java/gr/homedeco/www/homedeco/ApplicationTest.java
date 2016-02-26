37b7174ad68aa832350909be85526a55e92e78af
ChaliasosKonstantinos <chaliasos@gmail.com>
2016-02-26 21:58:56 +0200

    Initial commit

---
+++ app/src/androidTest/java/gr/homedeco/www/homedeco/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gr.homedeco.www.homedeco;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
