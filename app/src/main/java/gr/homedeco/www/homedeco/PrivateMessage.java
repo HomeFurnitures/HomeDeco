c4847c36598a9fc81a9f23ed65c80fe357147aff
ChaliasosKonstantinos <chaliasos@gmail.com>
2016-05-15 20:56:27 +0300

    ADDED: Login Activity Chat activity

---
+++ app/src/main/java/gr/homedeco/www/homedeco/PrivateMessage.java
@@ -0,0 +1,18 @@
+package gr.homedeco.www.homedeco;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class PrivateMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_message);
+    }
+
+    public void sendPrivateMessage(View view) {
+        
+    }
+}
