197e59ddc486f73246102a0028262ef43e003650
ChaliasosKonstantinos <chaliasos@gmail.com>
2016-05-15 20:00:24 +0300

    ADDED: User Registration Server Requests

--- app/src/main/java/gr/homedeco/www/homedeco/ServerRequests.java
+++ app/src/main/java/gr/homedeco/www/homedeco/ServerRequests.java
@@ -1,8 +1,10 @@
 package gr.homedeco.www.homedeco;
 
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +13,50 @@ public class ServerRequests {
 
     public static final String SERVER_ADDRESS = "http://www.compassbook.gr/";
     public static HttpURLConnection urlConnection;
+
+
+    //--------------------------------- ASYNC TASKS ----------------------------------------------//
+
+
+    //------------------------------ CONNECTION ESTABLISHERS -------------------------------------//
+
+    //Establishes a HttpURLConnection with GET as a RequestMethod
+    private HttpURLConnection openGetConnection(String uri, HashMap<String,String> dataToSend) {
+
+        try {
+            URL url = new URL(SERVER_ADDRESS + uri + getQueryData("GET", dataToSend));
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return urlConnection;
+    }
+
+    //Establishes a HttpURLConnection with POST as a RequestMethod
+    private HttpURLConnection openPostConnection(String uri) {
+
+        try {
+            URL url = new URL(SERVER_ADDRESS + uri);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setChunkedStreamingMode(0);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return urlConnection;
+    }
 
     //------------------------------------ QUERY DATA --------------------------------------------//
 
