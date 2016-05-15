4f379f30827ea834a717a4592c909a3ff050aa79
ChaliasosKonstantinos <chaliasos@gmail.com>
2016-05-15 21:00:46 +0300

    ADDED: Login Activity Chat activity

--- app/src/main/java/gr/homedeco/www/homedeco/PrivateMessage.java
+++ app/src/main/java/gr/homedeco/www/homedeco/PrivateMessage.java
@@ -3,16 +3,25 @@ package gr.homedeco.www.homedeco;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class PrivateMessage extends AppCompatActivity {
+
+    private EditText etMessage;
+    private ListView lvPrivateChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_message);
+
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        lvPrivateChat = (ListView) findViewById(R.id.lvPrivateChat);
     }
 
     public void sendPrivateMessage(View view) {
-        
+
+        String message = etMessage.getText().toString();
     }
 }
