6dbeddea2dcd69dfc00fc9151c0a9cdc525e4629
ChaliasosKonstantinos <chaliasos@gmail.com>
2016-10-28 20:55:23 +0300

    ADDED: Login activity Register activity User Model

---
+++ app/src/main/java/gr/homedeco/www/homedeco/Login.java
@@ -0,0 +1,31 @@
+package gr.homedeco.www.homedeco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private CheckBox cbRememberMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        cbRememberMe = (CheckBox) findViewById(R.id.cbRememberMe);
+    }
+
+    public void logIn(View view) {
+
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        User userToLogin = new User(username, password);
+    }
+}
--- app/src/main/java/gr/homedeco/www/homedeco/Main.java
+++ app/src/main/java/gr/homedeco/www/homedeco/Main.java
@@ -1,7 +1,9 @@
 package gr.homedeco.www.homedeco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Show Register Activity
+    public void showRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    //Show Login Activity
+    public void showLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
